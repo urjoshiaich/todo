ab6df40a38c5c90a003b2e9414c96b0e603a5df2
urjoshiaich <90167823+urjoshiaich@users.noreply.github.com>
2024-04-23 14:34:58 +0530

    adding files from local

---
+++ frontend/build/precache-manifest.a76a3b7091870a47cef779163482c399.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d822283afed771338cfc6d350b65073c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84c3f9b874346fc5a06b",
+    "url": "/static/css/2.1688c7e8.chunk.css"
+  },
+  {
+    "revision": "4f19ec846d87795bbf1d",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "84c3f9b874346fc5a06b",
+    "url": "/static/js/2.2c85da0e.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.2c85da0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f19ec846d87795bbf1d",
+    "url": "/static/js/main.3de7080e.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
---
+++ frontend/src/api/index.js
@@ -0,0 +1,11 @@
+export const url = "${process.env.ATLAS_URL}/api";
+
+export const setHeaders = () => {
+  const headers = {
+    headers: {
+      "x-auth-token": localStorage.getItem("token"),
+    },
+  };
+
+  return headers;
+};
